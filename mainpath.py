2cee10c9c4283dac5ac0b54aa4d326e9c4894242
Andrew Dillon <47433439+dillonal@users.noreply.github.com>
2019-04-24 21:34:03 -0400

    Update mainpath.py

--- mainpath.py
+++ mainpath.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 
-from ev3dev2.motor import LargeMotor, MediumMotor, MoveTank, OUTPUT_A, OUTPUT_B, OUTPUT_C  #  we're importing the motor function
+from ev3dev2.motor import LargeMotor, MediumMotor, MoveTank, MoveSteering, OUTPUT_A, OUTPUT_B, OUTPUT_C  #  we're importing the motor function
 from ev3dev2.motor import SpeedDPS, SpeedRPM, SpeedRPS, SpeedDPM #  we're importing other functions related to speed
 from ev3dev2.sensor.lego import TouchSensor, GyroSensor, UltrasonicSensor, ColorSensor
 from ev3dev2.sound import Sound
@@ -11,11 +11,14 @@ import os
 import sys
 import time
 import math
+from math import acos, cos, sin
+from textwrap import wrap
 
 lmR = LargeMotor('outA')
 lmL = LargeMotor('outB')
 lift = MediumMotor('outC')
 tank_pair = MoveTank(OUTPUT_A,OUTPUT_B)
+steer_pair = MoveSteering(OUTPUT_B,OUTPUT_A)
 
 cs = ColorSensor()
 us = UltrasonicSensor()
@@ -25,23 +28,27 @@ gy.MODE_GYRO_ANG
 
 sound = Sound()
 
+lcd = Display()
+
 # 1.4 rotations == 12 inches
 # 0.116667 rotations == 1 inch
+#sound.play_tone(1500, 1)
 
 const_speed = 25
 move_direction_counter = 0
 move_direction_counter_plus_minus = 0
-xCord = 0
-yCord = 0
+xCord = 6
+yCord = -6
 barcode_counter = 0
 
-
-def debug_print(*args, **kwargs):
-    '''Print debug messages to stderr.
-
-    This shows up in the output panel in VS Code.
-    '''
-    print(*args, **kwargs, file=sys.stderr)
+def show_text(string, font_name='courB24', font_width=15, font_height=24):
+    lcd.clear()
+    strings = wrap(string, width=int(180/font_width))
+    for i in range(len(strings)):
+        x_val = 89-font_width/2*len(strings[i])
+        y_val = 63-(font_height+1)*(len(strings)/2-i)
+        lcd.text_pixels(strings[i], False, x_val, y_val, font=font_name)
+    lcd.update()
 
 
 def move_bot(distance, direction): #distance in inches; true for direction = forward, flase for direction = backward
@@ -61,53 +68,40 @@ def move_bot(distance, direction): #distance in inches; true for direction = for
         move_direction_counter_plus_minus = 0
         tank_pair.on_for_rotations(-(const_speed),-(const_speed),wheel_rotations)
 
-    if (move_direction_counter_plus_minus == 1 and (move_direction_counter % 2 == 0 )):        #new
-        yCord = yCord + distance                                                              #new
-        #sound.play_tone(1500, 1)
-    elif (move_direction_counter_plus_minus == 0 and (move_direction_counter % 2 == 0 )):      #new
-        yCord = yCord - distance                                                              #new
-        #sound.play_tone(1500, 1)
-    elif (move_direction_counter_plus_minus == 1 and (move_direction_counter % 2 != 0)):       #new
-        xCord = xCord + distance                                                              #new
-        #sound.play_tone(1500, 1)
-    else:                                                                                      #new
-        xCord = xCord - distance                                                              #new
-        #sound.play_tone(1500, 1)
+    if (move_direction_counter_plus_minus == 1 and (move_direction_counter % 2 == 0 )):      
+        yCord = yCord + distance                                                              
+    elif (move_direction_counter_plus_minus == 0 and (move_direction_counter % 2 == 0 )):      
+        yCord = yCord - distance                                                             
+    elif (move_direction_counter_plus_minus == 1 and (move_direction_counter % 2 != 0)):      
+        xCord = xCord + distance                                                             
+    else:                                                                                     
+        xCord = xCord - distance                                                            
 
-    print('X CORD: ', str(xCord), '  ', ' Y CORD: ', str(yCord)) 
-    #print("Hello")
+    my_text = 'X CORD: ' + str(xCord)
+    show_text(my_text, 'courB14', 9, 14)
+    sleep(2)
+    my_text = 'Y CORD: ' + str(yCord)
+    show_text(my_text, 'courB14', 9, 14)
+    sleep(2)
 
     sleep(2)
 
 def turn_bot(direction):
     global move_direction_counter
-    move_direction_counter = move_direction_counter + 1 #new
-    sound.play_tone(1500, 1)
+    move_direction_counter = move_direction_counter + 1
     if (direction == "right"):
-        gy.MODE_GYRO_RATE
-        gy.MODE_TILT_ANG
-        angle = gy.angle
-        tempangle = gy.angle
-        while (abs(angle - tempangle) < 89):
-            tank_pair.on_for_seconds(-10,10,0.05)
-            angle = gy.angle
+        steer_pair.on_for_degrees(100,30,162)
     elif (direction == "left"):
-        gy.MODE_GYRO_RATE
-        gy.MODE_TILT_ANG
-        angle = gy.angle
-        tempangle = gy.angle
-        while (abs(angle - tempangle) < 89):
-            tank_pair.on_for_seconds(10,-10,0.05)
-            angle = gy.angle
+        steer_pair.on_for_degrees(-100,30,165)
 
     sleep(2)
 
 
 def lift_fork(direction):
     if (direction == "up"):
-        lift.on_for_seconds(speed = 10, seconds = 1)
+        lift.on_for_seconds(speed = 10, seconds = 1.1)
     if (direction == "down"):
-        lift.on_for_seconds(speed = -10, seconds = 1)
+        lift.on_for_seconds(speed = -10, seconds = 1.1)
 
     sleep(2)
 
@@ -120,61 +114,45 @@ def barcode_scan():                                         # true - Black; fals
     global yCord
     loop_counter = 0
 
-    #sound.speak("Loop Starting")
-    tank_pair.on(3,3)
+    tank_pair.on(5,5)
 
-    while ((readings != pattern) and (loop_counter < 85)):
-        #sound.speak("At the beginning")
+    while ((readings != pattern) and (loop_counter < 26)):
         loop_counter = loop_counter + 1
-        print(str(loop_counter))
         color = cs.color
-        #print(str(color))
-        sleep(0.75)
+        print(str(color))
+        sleep(0.51)
         if (color == 1 and pattern != readings):                                                    # change to match correct barcode index 1
             readings[0] = 1
-            #sound.speak("Index 1 correct")
-            #sound.speak(str(readings[0]))
             while (readings[0] == pattern[0]):
                 loop_counter = loop_counter + 1
-                print(str(loop_counter))
                 color = cs.color
-                #print(str(color))
-                sleep(0.75)
+                print(str(color))
+                sleep(0.51)
                 if (color != 1 and color != 0 and pattern != readings):                                            # change to match correct barcode index 2
                     readings[1] = 6
-                    #sound.speak("Index 2 correct")
-                    #sound.speak(str(readings[1]))
                     while (readings[1] == pattern[1]):
                         loop_counter = loop_counter + 1
-                        print(str(loop_counter))
                         color = cs.color
-                        #print(str(color))
-                        sleep(0.75)
+                        print(str(color))
+                        sleep(0.51)
                         if (color == 1 and pattern != readings):                                    # change to match correct barcode index 3
                             readings[2] = 1
-                            #sound.speak("Index 3 correct")
-                            #sound.speak(str(readings[2]))
                             while (readings[2] == pattern[2]):
                                 loop_counter = loop_counter + 1
-                                print(str(loop_counter))
                                 color = cs.color
-                                #print(str(color))
-                                sleep(0.75)
+                                print(str(color))
+                                sleep(0.51)
                                 if (color != 1 and color != 0):                            # change to match correct barcode index 4
                                     readings[3] = 6
                                     tank_pair.off(brake=True)
-                                    #sound.speak("Index 4 correct")
-                                    #sound.speak(str(readings[3]))
-                                    #sound.speak("This is the barcode in order")
-                                    #sound.speak("Index 1")
-                                    #sound.speak(str(readings[0]))
-                                    #sound.speak("Index 2")
-                                    #sound.speak(str(readings[1]))
-                                    #sound.speak("Index 3")
-                                    #sound.speak(str(readings[2]))
-                                    #sound.speak("Index 4")
-                                    #sound.speak(str(readings[3]))
                                     sound.speak("Barcode Matched")
+                                    lmL.on_for_rotations(-10,0.23)
+                                    lmR.on_for_rotations(-10,0.25)
+                                    turn_bot("left")
+                                    tank_pair.on_for_rotations(10,10,0.3)
+                                    lift_fork("up")
+                                    tank_pair.on_for_rotations(-10,-10,0.4)
+                                    turn_bot("right")
                                     break
 
 
@@ -206,38 +184,63 @@ def barcode_scan():                                         # true - Black; fals
                     break
 
     tank_pair.off(brake=True)
-    sound.speak("Completed")
-    #print(str(loop_counter))
     if (barcode_counter % 2 == 0 and readings != pattern):
         xCord = xCord + 48
     elif (barcode_counter % 2 != 0 and readings != pattern):
         xCord = xCord - 48
-    print('X CORD: ', str(xCord), '  ', ' Y CORD: ', str(yCord))
+
+    my_text = 'X CORD: ' + str(xCord)
+    show_text(my_text, 'courB14', 9, 14)
+    sleep(2)
+    my_text = 'Y CORD: ' + str(yCord)
+    show_text(my_text, 'courB14', 9, 14)
+    sleep(2)
+    
     barcode_counter = barcode_counter + 1
-    #sleep(60)
 
 
-def test():
-    print("hello")
+def iGPS():
+    radialDistanceA = float(input("Radial Distance from A: "))
+    radialDistanceC = float(input("Radial Distance from C: "))
+    radialDistanceD = float(input("Radial Distance from D: "))
+
+    b = 126
+
+    sound.speak("Click")
+
+    my_text = 'Radial Distance A: ' + str(radialDistanceA)
+    show_text(my_text, 'courB14', 9, 14)
+    sleep(2)
+    my_text = 'Radial Distance C: ' + str(radialDistanceC)
+    show_text(my_text, 'courB14', 9, 14)
+    sleep(2)
+    my_text = 'Radial Distance D: ' + str(radialDistanceD)
+    show_text(my_text, 'courB14', 9, 14)
+    sleep(2)
+
+    d = ((radialDistanceA * radialDistanceA) + ( b * b) - (radialDistanceC * radialDistanceC)) / (2 * radialDistanceA * b)
+    C = acos(d)
+    iGPSxCord = radialDistanceA * sin(C) - 6
+    iGPSyCord = radialDistanceA * cos(C) + 6
+
+    my_text = 'X Cordinate: ' + str(iGPSxCord)
+    show_text(my_text, 'courB14', 9, 14)
+    sleep(2)
+    my_text = 'Y Cordinate: ' + str(iGPSyCord)
+    show_text(my_text, 'courB14', 9, 14)
+    sleep(2)
 
 
+def Collision_Avoidance():
+    tank_pair.on(25,25)
+    distance = us.distance_centimeters_continuous
+    while distance > 40:
+        distance = us.distance_centimeters_continuous
 
 def main():
-
-    #move_bot(12,"forward")
-    #turn_bot("left")
-    #move_bot(15.7,"reverse")
-    #lift_fork("up")
-    #turn_bot("right")
-    #move_bot(28.6,"forward")
-    #turn_bot("left")
-    #move_bot(15.7,"forward")
-    #lift_fork("down")
-
-    barcode_scan()
-
-    #test()
-
+    #Input functions needed to complete the task at hand
+    
 if __name__ == "__main__":
     main()
 
+#brickrun -r ./Python_ev3_codes/mainpath.py
